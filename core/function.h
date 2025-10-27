49a82d20edef9522050b157b51ec7f6e5505d8f4
wuzhe_075 <wuzhe_075@qcraft.ai>
2025-10-27 10:18:38 +0800

    [gons]: add rosenbrok function

--- core/function.h
+++ core/function.h
@@ -7,7 +7,25 @@
 #include "vector.h"
 
 namespace gons {
-
+    template <typename T>
+    class Rosenbrock {
+    public:
+        Rosenbrock();
+        ~Rosenbrock();
+        
+        // Function evaluation
+        T operator()const Vector<T, 2> & x) const{
+            return (1 - x(0)) * (1 - x(0)) + 100 * (x(1) - x(0) * x(0)) * (x(1) - x(0) * x(0));
+        }
+        
+        // Gradient evaluation
+        Vector<T, 2> gradient(const Vector<T, 2>& x) const {
+            Vector<T, 2> grad;
+            grad(0) = -2 * (1 - x(0)) - 200 * x(0) * (x(1) - x(0) * x(0));
+            grad(1) = 200 * (x(1) - x(0) * x(0));
+            return grad;
+        }
+    };
 
 } // namespace gons
 
