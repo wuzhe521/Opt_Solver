d5c49b13e69a92a3051fe6696de3ed55b6abb859
wuzhe_075 <wuzhe_075@qcraft.ai>
2025-11-11 17:16:54 +0800

    [gons]: add ECQP test code

--- test/constrainedOpt.cc
+++ test/constrainedOpt.cc
@@ -9,20 +9,12 @@ int main() {
 
   Matrix<double, 2, 2> P = {{4.0, 1.0}, {1.0, 2.0}};
   Vector<double, 2> q = {1.0, 1.0};
-  Matrix<double, 3, 2> A = {{1.0, 1.0}, {1.0, 0.0}, {0.0, 1.0}};
-
-  Vector<double, 3> l = {1.0, 0.0, 0.0};
-  Vector<double, 3> u = {1.0, 0.7, 0.7};
-
-  Vector<double, 2> x = {0.0, 0.0};
-
-  /* penalty_function::QuadPenaltyFunction<double, 2, 3> PenaltyFunc(P, q, l, u,
-                                                                  A);*/
-  penalty_function::QuadPenaltyFunction<double, 2, 3>::function f(P, q, l, u,
-                                                                  A);
-
-  f(x);
-  f.gradient(x);
-  // PenaltyFunc.Optimize();
+  Matrix<double, 1, 2> A = {{1.0, 1.0}};
+  Vector<double, 1> b = {1.0};
+  penalty_function::QuadPenaltyFunction<double, 2, 1> PenaltyFunc(P, q, A, b);
+  PenaltyFunc.Optimize();
+  X rlt = PenaltyFunc.get_result();
+  std::cout << "result: " << rlt << std::endl;
+  
   return 0;
 }
