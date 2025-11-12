373b325cd902ffd1b79fb95623ba28da77c8c70c
wuzhe_075 <wuzhe_075@qcraft.ai>
2025-11-12 22:26:26 +0800

    [gons]: add ieqqp test code note: ieqqp under dev

--- test/constrainedOpt.cc
+++ test/constrainedOpt.cc
@@ -11,10 +11,20 @@ int main() {
   Vector<double, 2> q = {1.0, 1.0};
   Matrix<double, 1, 2> A = {{1.0, 1.0}};
   Vector<double, 1> b = {1.0};
-  penalty_function::QuadPenaltyFunction<double, 2, 1> PenaltyFunc(P, q, A, b);
+  penalty_function::QuadPenaltyFunction_ECQP<double, 2, 1> PenaltyFunc(P, q, A,
+                                                                       b);
   PenaltyFunc.Optimize();
   X rlt = PenaltyFunc.get_result();
   std::cout << "result: " << rlt << std::endl;
-  
+
+  Matrix<double, 2, 2> P2 = {{4.0, 1.0}, {1.0, 2.0}};
+  Vector<double, 2> q2 = {1.0, 1.0};
+  Matrix<double, 3, 2> A2 = {{1.0, 1.0}, {1.0, 0.0}, {0.0, 1.0}};
+  Vector<double, 3> l2 = {1.0, 0.0, 0.0};
+  Vector<double, 3> u2 = {1.0, 0.7, 0.7};
+
+  penalty_function::QuadPenaltyFunction_IECQP<double, 2, 3> PenaltyFunc2(P2, q2, A2, l2, u2);
+  PenaltyFunc2.Optimize();
+  std::cout << "result: " << PenaltyFunc2.get_result() << std::endl;
   return 0;
 }
