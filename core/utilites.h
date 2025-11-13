a3a1bf6a7921f38c65d9a5b976c6633044fa7c03
wuzhe_075 <wuzhe_075@qcraft.ai>
2025-11-13 15:11:43 +0800

    [gons]: fix LOG bug in diff platform

--- core/utilites.h
+++ core/utilites.h
@@ -4,7 +4,14 @@
 #include "config.h"
 #include <algorithm>
 #include <iostream>
-
+#ifdef _WIN32
+#include <stdio.h>
+#include <windows.h>
+void SetColor(int textColor, int bgColor = 0) {
+  HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+  SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
+}
+#endif
 namespace gons {
 namespace utilites {
 // LOG METHOD   need to be adapted according to platform
@@ -24,24 +31,24 @@ const char *const WARNING_PREFIX = "WARNING: ";
 #define LOG_WARNING(msg)
 
 #elif (LOG_LVL == 1)
-#ifdef __LINUX__
+#ifdef __linux__
 #define LOG(msg) SHW("\033[1;32m" << msg << "\033[0m\n");
 #define LOG_ERROR(msg) ERR("\033[1;31m" << msg << "\033[0m\n");
 #define LOG_WARNING(msg) WRN("\033[1;33m" << msg << "\033[0m\n");
 #endif
 #ifdef _WIN32
-#include <windows.h>
-#include <stdio.h>
-void SetColor(int textColor, int bgColor = 0) {
-HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
-}
-#define LOG(msg)  SHW(msg);
-#define LOG_ERROR(msg) SetColor(4); SHW(ERROR_PREFIX << msg << "\n"); SetColor(7);
-#define LOG_WARNING(msg) SetColor(6); SHW(WARNING_PREFIX << msg << "\n"); SetColor(7);
+#define LOG(msg) SHW(msg);
+#define LOG_ERROR(msg)                                                         \
+  SetColor(4);                                                                 \
+  SHW(ERROR_PREFIX << msg << "\n");                                            \
+  SetColor(7);
+#define LOG_WARNING(msg)                                                       \
+  SetColor(6);                                                                 \
+  SHW(WARNING_PREFIX << msg << "\n");                                          \
+  SetColor(7);
 #endif
 #elif (LOG_LVL == 2)
-
+#ifdef __linux__
 #define LOG(msg)                                                               \
   SHW("\033[1;32m[" << __FILE__ << ": L. " << __LINE__ << "] " << msg          \
                     << "\033[0m\n");
@@ -51,6 +58,23 @@ SetConsoleTextAttribute(hConsole, (bgColor << 4) | textColor);
 #define LOG_WARNING(msg)                                                       \
   WRN("\033[1;33m[" << __FILE__ << ": L. " << __LINE__ << "] " << msg          \
                     << "\033[0m\n");
+#endif
+#ifdef _WIN32
+#define LOG(msg)                                                               \
+  SetColor(4);                                                                 \
+  SHW("[" << __FILE__ << ": L. " << __LINE__ << "] " << msg << "\n");          \
+  SetColor(7);
+#define LOG_ERROR(msg)                                                         \
+  SetColor(6);                                                                 \
+  SHW("[" << __FILE__ << ": L. " << __LINE__ << "] " << ERROR_PREFIX << msg    \
+          << "\n");                                                            \
+  SetColor(7);
+#define LOG_WARNING(msg)                                                       \
+  SetColor(9);                                                                 \
+  SHW("[" << __FILE__ << ": L. " << __LINE__ << "] " << WARNING_PREFIX << msg  \
+                    << "\n");                                                  \
+  SetColor(7);
+#endif
 #else
 #error "Log Level Setting Invalid"
 #define LOG(msg)
